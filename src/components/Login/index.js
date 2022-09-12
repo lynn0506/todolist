80de7f05dc57e96825ec16234eca4847e664f139
정은주[Maps개발] <eunjo_o.jung@navercorp.com>
2022-09-13 00:01:15 +0900

    feat: 로그인 UI 구현

---
+++ src/components/Login/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+import './Login.scss';
+
+function Login() {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+    });
+
+    const onChangeFormData = (e) => {
+        e.preventDefault();
+
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    return (
+        <div className="template-container">
+            <div className="head-container">
+                <h1>로그인</h1>
+            </div>
+            <form className="login-form" >
+                <input
+                    autoFocus
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={formData.username}
+                    onChange={onChangeFormData}
+                    placeholder="아이디"
+                />
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={onChangeFormData}
+                    placeholder="비밀번호"
+                />
+                <button type="submit">로그인</button>
+                <button>회원가입</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
