4e30b031f80bcd11471d8c675af8d88c0bc62e76
정은주[Maps개발] <eunjo_o.jung@navercorp.com>
2022-09-13 00:00:57 +0900

    feat: 회원가입 UI 구현

---
+++ src/components/SignUp/index.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+
+import './SignUp.scss';
+
+function SignUp() {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+        passwordCheck: '',
+    });
+
+    const onChangeFormData = (e) => {
+        e.preventDefault();
+    
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    return (
+        <div className="template-container">
+            <div className="head-container">
+                <h1>회원가입</h1>
+            </div>
+            <form className="sign-up-form">
+                <label>
+                    아이디
+                    <input
+                        autoFocus
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={formData.username}
+                        onChange={onChangeFormData}
+                        placeholder="아이디"  
+                    />
+                </label>
+                <label>
+                    비밀번호
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={onChangeFormData}
+                        placeholder="6자리 이상의 비밀번호"
+                    />
+                </label>
+                <label>
+                    비밀번호 확인
+                    <input
+                        type="password"
+                        id="passwordCheck"
+                        name="passwordCheck"
+                        value={formData.passwordCheck}
+                        onChange={onChangeFormData}
+                        placeholder="비밀번호 확인"
+                    />
+                </label>
+                <button type="submit">회원가입 완료</button>
+                <button>로그인</button>
+            </form>        
+        </div>
+    );
+}
+
+export default SignUp;
